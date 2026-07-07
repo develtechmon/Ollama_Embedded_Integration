bae99ddc34c9d2e43359c348a339f880662b1dee
develtechmon <125783320+develtechmon@users.noreply.github.com>
2026-07-07 17:48:36 +0800

    Create ollama_serial_comm_esp32.ino

---
+++ Placeholder/ollama_serial_comm_esp32.ino
@@ -0,0 +1,42 @@
+#define LED_PIN 2
+#define MOTOR_PIN 18
+
+String command = "";
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(MOTOR_PIN, OUTPUT);
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    command = Serial.readStringUntil('\n');
+
+    command.trim();
+
+    if (command == "LED_ON")
+    {
+      digitalWrite(LED_PIN, HIGH);
+    }
+
+    else if (command == "LED_OFF")
+    {
+      digitalWrite(LED_PIN, LOW);
+    }
+
+    else if (command == "MOTOR_ON")
+    {
+      digitalWrite(MOTOR_PIN, HIGH);
+    }
+
+    else if (command == "MOTOR_OFF")
+    {
+      digitalWrite(MOTOR_PIN, LOW);
+    }
+  }
+}
